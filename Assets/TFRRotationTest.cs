bf2175306a0f2c649e9b27dadf87e4c1f34b33d3
Dean Day <dean@greenlightgames.co.uk>
2016-12-11 20:56:48 +0000

    - Increased Outer Box Scale to 101 - Moved Outer Box Slightly

---
+++ Assets/TFRRotationTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TFRRotationTest : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick));
+    }
+}
